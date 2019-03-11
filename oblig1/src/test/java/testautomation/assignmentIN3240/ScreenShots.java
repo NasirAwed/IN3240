52b95f368b4763b576cf3efa7ce9c4683ec7420e
byllgrim <robin.a.t.pedersen@gmail.com>
2019-03-11 11:44:57 +0100

    clean up comments

--- oblig1/src/test/java/testautomation/assignmentIN3240/ScreenShots.java
+++ oblig1/src/test/java/testautomation/assignmentIN3240/ScreenShots.java
@@ -10,10 +10,12 @@ import org.openqa.selenium.WebDriver;
 public class ScreenShots {
   public static String takeScreenshot(WebDriver driver, String fileName) throws IOException {
     fileName = fileName + ".png";
-    String directory = "iWasBornInTheGhetto";
+    String directory = "screenshots";
     File sourceFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
     FileUtils.copyFile(sourceFile, new File(directory + fileName));
     String destination = directory + fileName;
+
     return destination;
   }
 }
