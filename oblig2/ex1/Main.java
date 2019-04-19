c392ede895be9b0efd0d62b6448b48f31627c770
byllgrim <robin.a.t.pedersen@gmail.com>
2019-04-19 12:37:08 +0200

    calculate all primes up to N

--- oblig2/ex1/Main.java
+++ oblig2/ex1/Main.java
@@ -1,10 +1,10 @@
 class Main {
   public static void main(String[] args) {
     Primer p = new Primer();
-    int N = 10;
+    int N = 100;
     int[] primes;
 
-    primes = p.getPrimes(N);
+    primes = p.getPrimesUpTo(N);
     for (int i = 0; i < primes.length; i++)
       System.out.println(primes[i]);
   }
--- oblig2/ex1/Primer.java
+++ oblig2/ex1/Primer.java
@@ -1,20 +1,40 @@
 import java.util.Arrays;
 
 class Primer {
+  private boolean isDivisible(int a, int b) {
+    // TODO refactor out of this class
+
+    return a % b == 0;
+  }
+
   public boolean isPrime(int x) {
     return false; // TODO
   }
 
-  public int[] getPrimes(int N) {
+  public int[] getPrimesUpTo(int N) {
     boolean[] isPrimes;
     int[] primes;
+    int numFound;
 
     isPrimes = new boolean[N];
     Arrays.fill(isPrimes, true);
     isPrimes[0] = false;
     isPrimes[1] = false;
 
-    primes = new int[N]; // TODO smaller size
+    numFound = 0;
+    for (int i = 0; i < isPrimes.length; i++) {
+      if (!isPrimes[i])
+        continue;
+
+      numFound++;
+
+      for (int j = i + 1; j < isPrimes.length; j++) {
+        if (isDivisible(j, i))
+          isPrimes[j] = false;
+      }
+    }
+
+    primes = new int[numFound];
     int idx = 0; // TODO don't declare and init vars within body!
     for (int i = 0; i < isPrimes.length; i++) {
       if (isPrimes[i]) {
